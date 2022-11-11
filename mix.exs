00684187f13a7d2b5fce0e49b8a90c3b57f9b6e5
oegma2 <oegma2@gmail.com>
2022-11-11 01:25:16 +0100

    change elixir version to 12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AllStrings.MixProject do
     [
       app: :all_strings,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
